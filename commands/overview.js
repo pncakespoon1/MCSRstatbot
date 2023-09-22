86042e00a247a647774c7f7c50db70898dabce4d
pncakespoon1 <106548072+pncakespoon1@users.noreply.github.com>
2023-09-22 14:45:06 -0400

    added infrastructure, no functionality currently

--- commands/overview.js
+++ commands/overview.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+import { msToStr, roundToPerc } from '../helpers/formatting';
 
 module.exports = {
 	cooldown: 10,
@@ -7,16 +8,16 @@ module.exports = {
 		.setDescription('Shows basic info from a runner\'s stats.')
 		.addStringOption(option =>
 			option.setName('runner')
-			.setDescription('A spreadsheet id or the name of a runner')
-			.setRequired(true))
+				.setDescription('A spreadsheet id or the name of a runner')
+				.setRequired(true))
 		.addStringOption(option =>
 			option.setName('session')
-			.setDescription('Choose the session (currently just latest or all)')
-			.setRequired(false)),
-			.addChoices(
-			{ name: "latest", value: "latest"},
-			{ name: "all", value: "all"}
-			)),
+				.setDescription('Choose the session (currently just latest or all)')
+				.setRequired(false))
+		.addChoices(
+			{ name: "latest", value: "latest" },
+			{ name: "all", value: "all" }
+		),
 	async execute(interaction) {
 		const session = interaction.options.getString('session');
 		const runner = interaction.options.getRunner('session');
@@ -25,13 +26,27 @@ module.exports = {
 		const link = `${website_link}/api/sheet/${id}`
 
 		fetch(link)
-		.then((data) => {
-		    const sess_data = (session === "latest" ? data["session"][0]["ops"] : data["overall"])
+			.then((res) => {
+				console.log(res.ok);
+				return res.json()
+			})
+			.then((data) => {
+				const sess_data = (session === "latest" ? data["session"][0]["ops"] : data["overall"])
+				const fields = [
+					{ name: "rnph", value: String(roundToPerc(sess_data["rnph"])) },
+					{ name: "fnph", value: String(roundToPerc(sess_data["fnph"])) },
+					{ name: "enter avg", value: msToStr(sess_data["tl"][3]["time"]) },
+					{ name: "playtime", value: msToStr(sess_data["tp"]) },
+					{ name: "seeds played %", value: `${roundToPerc(roundToPerc(sess_data["pc"] / sess_data["rc"], 4) * 100)}%` },
+					{ name: "reset count", value: String(sess_data["rc"]) }
+				]
+				const embed = new EmbedBuilder()
+					.setTitle(`Stats for ${(runner.length > 20 ? "runner" : runner)}`)
+					.setURL(`${website_link}/sheet/${link}`)
+					.setDescription("basic overview stats")
+					.addFields(...fields)
+				interaction.reply({ embers: [embed] })
 
-        const embed = new EmbedBuilder()
-        .setTitle(`Stats for ${runner.length > 20 ? "runner" ? runner}`)
-        .setURL(`${website_link}/sheet/${link}`)
-  }
-		})
-	},
-};
+			})
+	}
+}
---
+++ helpers/formatting.js
@@ -0,0 +1,19 @@
+export const msToStr = (ms, dp = true) => {
+    let deciseconds = Math.floor((ms % 1000) / 100),
+      seconds = Math.floor((ms / 1000) % 60),
+      minutes = Math.floor((ms / (1000 * 60)) % 60),
+      hours = Math.floor((ms / (1000 * 60 * 60)));
+  
+  
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10 && hours !== "00") ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+    if (hours === "00") {
+      return minutes + ":" + seconds + (dp ? "." + deciseconds : "")
+    } else {
+      return hours + ":" + minutes + ":" + seconds + (dp ? "." + deciseconds : "")
+    }
+  }
+  
+export const roundToPerc = (fullNum, digits = 2) => Math.round(fullNum * (10 ** digits)) / (10 ** digits)
