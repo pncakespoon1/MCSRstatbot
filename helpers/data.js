031f79d144813d42d9eac0b319faff4c170cffc1
pncakespoon1 <106548072+pncakespoon1@users.noreply.github.com>
2023-09-25 12:11:39 -0400

    Update data.js

--- helpers/data.js
+++ helpers/data.js
@@ -1,5 +1,5 @@
 module.exports = {
-    website_link: "https://reset-analytics-dev.vercel.app",
+    website_link: "https://reset-analytics.vercel.app",
     timelines: ["Iron", "Wood", "Iron Pickaxe", "Nether", "Bastion", "Fortress", "Nether Exit", "Stronghold", "End"],
     colourList: ["#0088FE", "#00C49F", "#973e95", "#FF8042", "#FFC0CB", "#FFD700"]
 }
